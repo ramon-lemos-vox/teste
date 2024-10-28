a568cdb936d5bc61a111fa2034119046bd6b0aa3
Ramon <johnnyramon2011@hotmail.com>
2024-10-28 16:06:23 -0300

    users

--- Models/db/ConnectionDB.js
+++ Models/db/ConnectionDB.js
@@ -7,7 +7,7 @@ class ConnectionDB {
       user: process.env.DB_USER,
       database: process.env.DB_DATABASE,
       password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
+      // port: process.env.DB_PORT,
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
